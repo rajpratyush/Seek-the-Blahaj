a71be57103db5e9956586e8c026ecea0f2123f97
unknown <500075823@stu.upes.ac.in>
2021-05-30 15:11:00 +0530

    Some map tweaks

--- levels/ryans_room/events.js
+++ levels/ryans_room/events.js
@@ -1,5 +1,11 @@
 module.exports = function(event, world) {
-  console.log(`VR Template: ${event.name}`);
+  console.log('VR Template: ${event.name}');
   console.log(event);
   console.log(world);
+  if (
+    event.name === "playerDidInteract" &&
+    event.target.key === "lever1"
+  ) {
+    world.showNotification('Blahaj is not here!');
+  }
 }
